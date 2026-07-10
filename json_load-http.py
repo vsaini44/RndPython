915617cc877be97492d08a804dc4552c563cda5e
vsaini44 <vishal@ap2v.com>
2026-07-11 01:42:24 +0530

    Create json_load-http.py

---
+++ json_load-http.py
@@ -0,0 +1,24 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+
+class abchandler(BaseHTTPRequestHandler):
+    
+    def do_POST(self):
+        length = int(self.headers["Content-Length"])
+        body = self.rfile.read(length)          # data sent by client
+        
+        data = json.loads(body)
+        response = {
+            "message": "hello " + data["servername"]
+        }
+         
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+
+        self.wfile.write(json.dumps(response).encode())
+
+server=HTTPServer(("localhost",8081),abchandler)
+print("server started on localhost:8081")
+
+server.serve_forever()
